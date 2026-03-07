04e15eff264459141b84cd2f8ba698e3f03c8e23
Tomas Figueiredo <dev.tomas2003@gmail.com>
2026-03-07 00:03:52 +0000

    Raven Support

---
+++ src/raven.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const CONTROLPLANE_BASE = "http://api.raven.slabs.pt";
+
+export async function SendMail(outboxId, from, fromName, subject, text, html, cc, bcc, to) {
+    if (!outboxId || !from || !fromName || !subject || (!text && !html) || (cc && !Array.isArray(cc)) || (bcc && !Array.isArray(bcc)) || (to && !Array.isArray(to))) {
+        throw new TypeError("SendMail: Missing required fields. Check the documentation for the correct format.");
+    }
+
+    try {
+        const objRes = await axios.post(`http://${CONTROLPLANE_BASE}.slabs.pt/sendMail`, {
+            outboxId,
+            from,
+            fromName,
+            subject,
+            text,
+            html,
+            cc,
+            bcc,
+            to
+        },
+        {
+            headers: {
+                Authorization: "Bearer " + process.env.STS_CODE
+            },
+            timeout: 10000
+        });
+
+        return {
+            success: true
+        };
+    } catch (err) {
+        console.error(`SendMail(${objectUrn}) failed:`, err?.message || err);
+        throw err;
+    }
+}
---
+++ src/sdk.js
@@ -0,0 +1,7 @@
+import * as Bytelake from "./bytelake.js";
+import * as Raven from "./raven.js";
+
+export {
+  Bytelake,
+  Raven
+};
